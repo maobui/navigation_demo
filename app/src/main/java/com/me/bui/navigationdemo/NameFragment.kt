2d30b68adc82cf031c33febbf547fb97742ab29b
maobui <mao.buidinh@hotmail.com>
2019-06-04 17:55:48 +0700

    - Navigation with args.

--- app/src/main/java/com/me/bui/navigationdemo/NameFragment.kt
+++ app/src/main/java/com/me/bui/navigationdemo/NameFragment.kt
@@ -6,26 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_account.*
+import kotlinx.android.synthetic.main.fragment_name.*
 
-class AccountFragment : Fragment() {
+
+class NameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        return inflater.inflate(R.layout.fragment_name, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btn_next.setOnClickListener {
-            val nameBundle = Bundle()
-            nameBundle.putString("nameArg", edt_name.text.toString())
-            it.findNavController().navigate(R.id.nameFragment, nameBundle)
-        }
+
+        txt_name.text = arguments?.getString("nameArg")
     }
+
+
 }
